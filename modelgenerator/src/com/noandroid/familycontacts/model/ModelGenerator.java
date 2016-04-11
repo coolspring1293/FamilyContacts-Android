b45639e7de3210278e70cc8f5b4eee35dae282fe
leasunhy <leasunhy@gmail.com>
2016-04-12 02:47:30 +0800

    add some comments

--- modelgenerator/src/com/noandroid/familycontacts/model/ModelGenerator.java
+++ modelgenerator/src/com/noandroid/familycontacts/model/ModelGenerator.java
@@ -9,18 +9,21 @@ public class ModelGenerator {
     public static void main(String[] args) throws Exception {
         Schema schema = new Schema(1000, "com.noandroid.familycontacts.model");
 
+        // Record
         Entity record = schema.addEntity("Record");
         record.addIdProperty();
         record.addDateProperty("time").notNull();
         record.addIntProperty("status").notNull();
         record.addStringProperty("telephoneNumber").notNull();
 
+        // Contact
         Entity contact = schema.addEntity("Contact");
         contact.addIdProperty().getProperty();
         contact.addStringProperty("name").notNull();
         contact.addStringProperty("relationship");
         contact.addStringProperty("avatar");
 
+        // City
         Entity city = schema.addEntity("City");
         city.addIdProperty().getProperty();
         city.addStringProperty("province").notNull();
@@ -29,15 +32,18 @@ public class ModelGenerator {
         city.addStringProperty("weatherInfo");
         city.addStringProperty("temperature");
 
+        // Telephone
         Entity telephone = schema.addEntity("Telephone");
         Property telephoneOriId = telephone.addIdProperty().getProperty();
         telephone.addStringProperty("number").index();
         Property contactId = telephone.addLongProperty("contactId").getProperty();
         Property cityId = telephone.addLongProperty("cityId").getProperty();
 
-        telephone.addToOne(contact, contactId);
+        // Relationship: Telephone-City
         telephone.addToOne(city, cityId);
 
+        // Relationship: Telephone-Contact
+        telephone.addToOne(contact, contactId);
         ToMany contactToTelephones = contact.addToMany(telephone, contactId);
         contactToTelephones.setName("telephones");
         contactToTelephones.orderAsc(telephoneOriId);
