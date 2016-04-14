a3825fc8295ccd552cbd6610cfd60de7e8e458bc
guanlu <470616724@qq.com>
2016-04-14 19:31:01 +0800

    ExpandableList test

--- MyApplication/app/src/main/java/com/example/admin/myapplication/ContactsActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/ContactsActivity.java
@@ -1,23 +1,42 @@
 package com.example.admin.myapplication;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 /**
  * Created by 关璐 on 2016/3/31.
  */
-public class ContactsActivity extends Fragment {
+public class ContactsActivity extends Fragment{
+
     private View mParent;
 
     private FragmentActivity mActivity;
-
-    private TextView mText;
-
     public static ContactsActivity newInstance(int index) {
         ContactsActivity f = new ContactsActivity();
 
@@ -36,15 +55,24 @@ public class ContactsActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_contacts, container,false);
+
+
         return view;
     }
+
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mActivity = getActivity();
         mParent=getView();
+        /*StickyListHeadersListView stickyList =(StickyListHeadersListView)mParent.findViewById(R.id.contact_list);
+        MyAdapter adapter = new MyAdapter(getActivity());
+        stickyList.setAdapter(adapter);*/
+
 
         //mText = (TextView)mParent.findViewById(R.id.fragment_contacts);
 
@@ -59,4 +87,6 @@ public class ContactsActivity extends Fragment {
     public void onDestroy() {
         super.onDestroy();
     }
+
+
 }
--- MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,23 +1,193 @@
 package com.example.admin.myapplication;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.WeakHashMap;
+
+import se.emilsjolander.stickylistheaders.ExpandableStickyListHeadersListView;
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 public class MainActivity extends FragmentActivity {
     public static Fragment[] mFragments;
     FragmentIndicator mIndicator;
+
+    private ExpandableStickyListHeadersListView mListView;
+    TestBaseAdapter mTestBaseAdapter;
+    WeakHashMap<View,Integer> mOriginalViewHeightPool = new WeakHashMap<View, Integer>();
+
+    //animation executor
+    class AnimationExecutor implements ExpandableStickyListHeadersListView.IAnimationExecutor {
+
+        @Override
+        public void executeAnim(final View target, final int animType) {
+            if(ExpandableStickyListHeadersListView.ANIMATION_EXPAND==animType&&target.getVisibility()==View.VISIBLE){
+                return;
+            }
+            if(ExpandableStickyListHeadersListView.ANIMATION_COLLAPSE==animType&&target.getVisibility()!=View.VISIBLE){
+                return;
+            }
+            if(mOriginalViewHeightPool.get(target)==null){
+                mOriginalViewHeightPool.put(target,target.getHeight());
+            }
+            final int viewHeight = mOriginalViewHeightPool.get(target);
+            float animStartY = animType == ExpandableStickyListHeadersListView.ANIMATION_EXPAND ? 0f : viewHeight;
+            float animEndY = animType == ExpandableStickyListHeadersListView.ANIMATION_EXPAND ? viewHeight : 0f;
+            final ViewGroup.LayoutParams lp = target.getLayoutParams();
+            ValueAnimator animator = ValueAnimator.ofFloat(animStartY, animEndY);
+            animator.setDuration(200);
+            target.setVisibility(View.VISIBLE);
+            animator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    if (animType == ExpandableStickyListHeadersListView.ANIMATION_EXPAND) {
+                        target.setVisibility(View.VISIBLE);
+                    } else {
+                        target.setVisibility(View.GONE);
+                    }
+                    target.getLayoutParams().height = viewHeight;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    lp.height = ((Float) valueAnimator.getAnimatedValue()).intValue();
+                    target.setLayoutParams(lp);
+                    target.requestLayout();
+                }
+            });
+            animator.start();
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       /* StickyListHeadersListView stickyList =(StickyListHeadersListView)findViewById(R.id.contact_list);
+        MyAdapter adapter = new MyAdapter(this);
+        stickyList.setAdapter(adapter);*/
 
+        mListView = (ExpandableStickyListHeadersListView) findViewById(R.id.contact_list);
+        //custom expand/collapse animation
+        mListView.setAnimExecutor(new AnimationExecutor());
+        mTestBaseAdapter = new TestBaseAdapter(this);
+        mListView.setAdapter(mTestBaseAdapter);
+        mListView.setOnHeaderClickListener(new StickyListHeadersListView.OnHeaderClickListener() {
+            @Override
+            public void onHeaderClick(StickyListHeadersListView l, View header, int itemPosition, long headerId, boolean currentlySticky) {
+                if (mListView.isHeaderCollapsed(headerId)) {
+                    mListView.expand(headerId);
+                } else {
+                    mListView.collapse(headerId);
+                }
+            }
+        });
         setFragmentIndicator(0);
     }
+
+    public class MyAdapter extends BaseAdapter implements StickyListHeadersAdapter {
+
+        private String[] countries = {"America","China","Japan","England"};
+        private LayoutInflater inflater;
+
+        public MyAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return countries.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return countries[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.test_list_item_layout, parent, false);
+                holder.text = (TextView) convertView.findViewById(R.id.text);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.text.setText(countries[position]);
+
+            return convertView;
+        }
+
+        @Override
+        public View getHeaderView(int position, View convertView, ViewGroup parent) {
+            HeaderViewHolder holder;
+            if (convertView == null) {
+                holder = new HeaderViewHolder();
+                convertView = inflater.inflate(R.layout.header, parent, false);
+                holder.text = (TextView) convertView.findViewById(R.id.text);
+                convertView.setTag(holder);
+            } else {
+                holder = (HeaderViewHolder) convertView.getTag();
+            }
+            //set header text as first char in name
+            String headerText = "" + countries[position].subSequence(0, 1).charAt(0);
+            holder.text.setText(headerText);
+            return convertView;
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            //return the first character of the country as ID because this is what headers are based upon
+            return countries[position].subSequence(0, 1).charAt(0);
+        }
+
+        class HeaderViewHolder {
+            TextView text;
+        }
+
+        class ViewHolder {
+            TextView text;
+        }
+
+    }
+
     private void setFragmentIndicator(int whichIsDefault) {
         mFragments = new Fragment[4];
         mFragments[0] = getSupportFragmentManager().findFragmentById(R.id.fragment_contacts);
@@ -43,6 +213,7 @@ public class MainActivity extends FragmentActivity {
         });
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -64,4 +235,5 @@ public class MainActivity extends FragmentActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
