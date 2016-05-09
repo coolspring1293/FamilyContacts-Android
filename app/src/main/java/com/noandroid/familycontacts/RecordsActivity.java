8c84886e41557c87da2d72b14f4b7ee2f1ba4109
leasunhy <leasunhy@gmail.com>
2016-05-09 14:36:50 +0800

    finish Record fragment

--- app/src/main/java/com/noandroid/familycontacts/RecordsActivity.java
+++ app/src/main/java/com/noandroid/familycontacts/RecordsActivity.java
@@ -1,18 +1,29 @@
 package com.noandroid.familycontacts;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+import com.noandroid.familycontacts.model.ContactDao;
+import com.noandroid.familycontacts.model.RecordDao;
+import com.noandroid.familycontacts.model.Telephone;
+import com.noandroid.familycontacts.model.TelephoneDao;
+
+import static com.noandroid.familycontacts.MainActivity.*;
 
 
 /**
  * Created by 关璐 on 2016/3/31.
+ * Edited by leasunhy on 2016/5/9
  */
-public class RecordsActivity extends Fragment {
+public class RecordsActivity extends Fragment implements View.OnClickListener {
     private View mParent;
 
     private FragmentActivity mActivity;
@@ -60,4 +71,50 @@ public class RecordsActivity extends Fragment {
     public void onDestroy() {
         super.onDestroy();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateListContent();
+    }
+
+    private void updateListContent() {
+        String contactQueryStr =
+                "SELECT " +
+                " contact._id as contactid, contact.name as display_name, count(*) as count, record.time, record.status, telephone.number " +
+                " FROM " + " record left outer join telephone natural join contact " +
+                " WHERE record.telephone_number = telephone.number AND contact._id is not null " +
+                " GROUP BY " + " contact._id";
+        String telephoneQueryStr =
+                "SELECT " +
+                " -1 as contactid, telephone_number as display_name, count(*) as count, time, status, telephone_number as number " +
+                " FROM " + " record " +
+                " WHERE " + " telephone_number not in (SELECT number FROM telephone) " +
+                " GROUP BY " + " telephone_number";
+        String queryStr =
+                "SELECT _ROWID_ as _id, * " +
+                " FROM " + "(" + contactQueryStr + " UNION ALL " + telephoneQueryStr + ")" + " ORDER BY " + "time DESC";
+        //String[] from = { "display_name", "count", "record.time", "record.status", "telephone.number" };
+        Cursor cursor = db.rawQuery(queryStr, null);
+        RecordCursorAdapter adapter = new RecordCursorAdapter(getContext(), cursor, 0, this);
+        ((ListView)getView().findViewById(android.R.id.list)).setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Long contactid = ((RecordCursorAdapter.ViewHolder)view.getTag()).contactid;
+        if (contactid < 0) {  // stranger
+            // TODO(leasunhy): call log detail page for strangers
+            return;
+        } else {
+            Intent intent = new Intent();
+            intent.setClass(getContext(), ContactDetailsActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("contactId", contactid.toString());
+            intent.putExtras(bundle);
+            startActivityForResult(intent, 1);
+        }
+    }
 }
+
